f3ff696df9b27603597a4fe783df74d275c42922
Meda Serenaite <meda.serenaite@gmail.com>
2023-03-29 11:31:02 -0500

    adds sample day set up dir

---
+++ sampleDaySetUp/index.js
@@ -0,0 +1,21 @@
+// https://adventofcode.com/2021/day/1 //DELETE
+// https://adventofcode.com/ //FILL URL
+
+//save input file
+//update file route
+
+const input = require("fs").readFileSync("20../Day../input.txt", "UTF-8").replace(/\r/g, "").split("\n").map(Number)
+
+//   P1 
+let part1Answer;
+
+
+
+console.log(`Part 1: ${part1Answer}`)
+
+//  P2
+let part2Answer;
+
+
+
+console.log(`Part 2: ${part2Answer}`)
